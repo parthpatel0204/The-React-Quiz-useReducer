7de416564c1c65a3c74eb145a20e6fd4075251ac
Parth Patel <parthvishnubhai38@gmail.com>
2025-04-27 11:12:23 +0530

    Today's changes
    - Shifted reducer function and initialState in the global scope

--- src/App.jsx
+++ src/App.jsx
@@ -4,48 +4,48 @@ import StartScreen from "./components/StartScreen";
 import Questions from "./components/Questions";
 import EndScreen from "./components/EndScreen";
 
-function App() {
-  const initialState = {
-    question: [],
-    start: false,
-    currentQuestion: 0,
-    point: 0,
-    selectedAnswers: [],
-  };
+const initialState = {
+  question: [],
+  start: false,
+  currentQuestion: 0,
+  point: 0,
+  selectedAnswers: [],
+};
 
-  const [state, dispatch] = useReducer(reducer, initialState);
+function reducer(state, action) {
+  switch (action.type) {
+    case "allQuestion":
+      return { ...state, question: action.payload };
 
-  function reducer(state, action) {
-    switch (action.type) {
-      case "allQuestion":
-        return { ...state, question: action.payload };
+    case "startQuiz":
+      return {
+        ...state,
+        start: true,
+      };
 
-      case "startQuiz":
-        return {
-          ...state,
-          start: true,
-        };
+    case "nextQuestion":
+      return { ...state, currentQuestion: state.currentQuestion + 1 };
 
-      case "nextQuestion":
-        return { ...state, currentQuestion: state.currentQuestion + 1 };
+    case "backQuestion":
+      return { ...state, currentQuestion: state.currentQuestion - 1 };
 
-      case "backQuestion":
-        return { ...state, currentQuestion: state.currentQuestion - 1 };
+    case "questionPoint":
+      return { ...state, point: state.point + action.payload };
 
-      case "questionPoint":
-        return { ...state, point: state.point + action.payload };
+    case "selectAnswer": {
+      const updated = [...state.selectedAnswers];
+      updated[state.currentQuestion] = action.payload;
 
-      case "selectAnswer": {
-        const updated = [...state.selectedAnswers];
-        updated[state.currentQuestion] = action.payload;
-
-        return { ...state, selectedAnswers: updated };
-      }
-
-      default:
-        return state;
+      return { ...state, selectedAnswers: updated };
     }
+
+    default:
+      return state;
   }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(function () {
     fetch("http://localhost:9000/questions")
